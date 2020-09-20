9ed166c8f4fc9e4bf48457486a38074dbda8d809
Julian Rich <s3407957@student.rmit.edu.au>
2020-09-20 17:44:53 +1000

    Initial Commit

---
+++ autoLayoutExample/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autoLayoutExample
+//
+//  Created by Julian Rich on 20/9/20.
+//  Copyright © 2020 Julian Rich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
